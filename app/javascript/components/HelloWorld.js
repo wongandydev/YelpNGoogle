706c7ce67ae28b25b56406125db72ae7c1121012
Andy Wong <andy.wong@plated.com>
2018-06-08 14:24:12 -0400

    init commit

---
+++ app/javascript/components/HelloWorld.js
@@ -0,0 +1,21 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          The Yelp + Maps Tool
+        </h1>
+        <form>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default HelloWorld
+
+
+
