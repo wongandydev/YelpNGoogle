726eee135347be1bbf67649c68ab689b85f5c498
Andy Wong <andy.wong@plated.com>
2018-06-12 16:29:56 -0400

    remove Ziplooker API call

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,15 @@
 class HomeController < ApplicationController
-  Coordinates = Struct.new(:lat, :long)
 
   def index
     # @coordinates = Coordinates.new(body["lat"], body["lng"])
   end
 
   def apiCall
-    binding.pry
-    uri = "https://www.zipcodeapi.com/rest/jyxRYlYW9WIiXTZgBppSKmUqzxsb8FTeTHZZe4d5cZIFwPZijs3UrvYvJGrlvPt1/info.json/#{zip_code}/degrees"
-    response = HTTParty.get(uri)
-    body = JSON.parse(response.body)
-    return uri
+    # # binding.pry
+    # zip_code = ""
+    # uri = "https://www.zipcodeapi.com/rest/jyxRYlYW9WIiXTZgBppSKmUqzxsb8FTeTHZZe4d5cZIFwPZijs3UrvYvJGrlvPt1/info.json/#{zip_code}/degrees"
+    # response = HTTParty.get(uri)
+    # body = JSON.parse(response.body)
+    # return {"this": 2}
   end
 end
